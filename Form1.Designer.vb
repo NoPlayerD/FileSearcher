1dcc28fb3bbb4ad9d1f0f0a586b846bbc2af12df
NoPlayer.D <jadonredninja@gmail.com>
2023-02-25 00:59:51 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Get Files"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(73, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(526, 407)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(104, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 29)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Get Folders"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 429)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(846, 18)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "ByName:"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Button3.Location = New System.Drawing.Point(213, 19)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(92, 29)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Get Both"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(547, 38)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(311, 381)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(73, 88)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 12
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(11, 124)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(167, 20)
+        Me.CheckBox1.TabIndex = 11
+        Me.CheckBox1.Text = "Include sub files/folders"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(11, 346)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(42, 29)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "Clear"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label3.Location = New System.Drawing.Point(175, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Example: .txt / .exe"
+        Me.Label3.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label2.Location = New System.Drawing.Point(8, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 16)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "ByExt:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(544, 12)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(241, 20)
+        Me.TextBox2.TabIndex = 9
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(791, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(67, 20)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Browse"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(866, 456)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "File Searcher"
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim path As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If CheckBox1.Checked = True Then getfile(1) Else getfile(0)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If CheckBox1.Checked = True Then getdir(1) Else getdir(0)
+    End Sub
+    Private Function getfile(ctrl As Integer)
+        Try
+            ListBox1.Items.Clear()
+
+            If ctrl = 1 Then
+                Dim txtFilesArray As String() = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                Dim max As Int64 = txtFilesArray.Count
+                ProgressBar1.Maximum = max
+
+                For i As Integer = 0 To txtFilesArray.Count - 1
+                    ListBox1.Items.Add(txtFilesArray(i))
+                    ProgressBar1.Value = i + 1
+                Next
+
+            Else
+                Dim txtFilesArray As String() = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+                Dim max As Int64 = txtFilesArray.Count
+                ProgressBar1.Maximum = max
+
+                For i As Integer = 0 To txtFilesArray.Count - 1
+                    ListBox1.Items.Add(txtFilesArray(i))
+                    ProgressBar1.Value = i + 1
+                Next
+            End If
+        Catch ex As Exception
+            MsgBox("Error..")
+        End Try
+
+        Return True
+    End Function
+
+    Private Function getdir(ctrl As Integer)
+        Try
+            ListBox1.Items.Clear()
+            If ctrl = 1 Then
+                GetSubDirectories()
+            Else
+                Dim dir As String() = Directory.GetDirectories(path)
+                For Each folder As String In dir
+                    ListBox1.Items.Add(folder)
+                Next
+            End If
+        Catch ex As Exception
+            MsgBox("Error..")
+        End Try
+
+        Return True
+    End Function
+    Private Function both(ctrl As Integer)
+        Try
+            ListBox1.Items.Clear()
+
+            If ctrl = 1 Then
+                Dim txtFilesArray As String() = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                Dim max As Int64 = txtFilesArray.Count
+                ProgressBar1.Maximum = max
+
+                For i As Integer = 0 To txtFilesArray.Count - 1
+                    ListBox1.Items.Add(txtFilesArray(i))
+                    ProgressBar1.Value = i + 1
+                Next
+                GetSubDirectories()
+
+            Else
+
+                Dim txtFilesArray As String() = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+                Dim max As Int64 = txtFilesArray.Count
+                ProgressBar1.Maximum = max
+
+                For i As Integer = 0 To txtFilesArray.Count - 1
+                    ListBox1.Items.Add(txtFilesArray(i))
+                    ProgressBar1.Value = i + 1
+                Next
+                Dim dir As String() = Directory.GetDirectories(path)
+                For Each folder As String In dir
+                    ListBox1.Items.Add(folder)
+                Next
+            End If
+
+        Catch ex As Exception
+            MsgBox("Error..")
+        End Try
+
+        Return True
+    End Function
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim fb As New FolderBrowserDialog
+        Dim folder As String
+        fb.ShowDialog()
+
+        If Not fb.SelectedPath = vbNullString Or vbNull Then
+            folder = fb.SelectedPath
+            TextBox2.Text = folder
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        path = TextBox2.Text
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ListBox1.Items.Clear()
+        ProgressBar1.Value = 0
+    End Sub
+    Public Sub GetSubDirectories()
+        Dim subdirectoryEntries As String() = Directory.GetDirectories(path)
+        For Each subdirectory As String In subdirectoryEntries
+            LoadSubDirs(subdirectory)
+        Next
+    End Sub
+    Private Sub LoadSubDirs(dir As String)
+        ListBox1.Items.Add(dir)
+        Dim subdirectoryEntries As String() = Directory.GetDirectories(dir)
+        For Each subdirectory As String In subdirectoryEntries
+            LoadSubDirs(subdirectory)
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If CheckBox1.Checked = True Then both(1) Else both(0)
+    End Sub
+
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        If Me.Width > 882 Or Me.Width < 882 Then
+            Me.Width = 882
+        End If
+    End Sub
+
+    Private Sub TextBox3_GotFocus(sender As Object, e As EventArgs) Handles TextBox3.GotFocus
+        Label3.Visible = True
+    End Sub
+
+    Private Sub TextBox3_Leave(sender As Object, e As EventArgs) Handles TextBox3.Leave
+        Label3.Visible = False
+    End Sub
+End Class
